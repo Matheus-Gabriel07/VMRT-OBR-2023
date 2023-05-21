e64e30b386e3acf0184a5f88ed2b0d9dabdc0c2a
Matheus Gabriel <math.gsilva07@gmail.com>
2023-05-21 15:54:36 -0300

    Summary

---
+++ Tests/Buttons.ino
@@ -0,0 +1,19 @@
+void Buttons () {
+  
+  int btnF1 = none;
+  int btnF2 = none;
+  int btnF3 = none;
+  int btnF4 = none;
+  int btnState = 0;
+  
+  void setup () {
+    pinMode(btnF1, INPUT);
+    pinMode(btnF2, INPUT);
+    pinMode(btnF3, INPUT);
+    pinMode(btnF4, INPUT);
+  }
+
+  void loop () {
+    
+  }
+}
