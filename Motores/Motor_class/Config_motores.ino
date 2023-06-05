81b422e302c6f22a8d87de511dc97e6849bbac12
Matheus Gabriel <math.gsilva07@gmail.com>
2023-06-05 10:05:43 -0300

    Fix: Update Config_motors

--- Motores/Motor_class/Config_motores.ino
+++ Motores/Motor_class/Config_motores.ino
@@ -15,13 +15,13 @@ void statico () {
   motorRight.move(0);
 }
 
-void curvaEsq(int move) { //Curva para esquerda em um eixo
-  motorLeft.move(move);
+void curvaEsq (int move) { //Curva para esquerda em um eixo
+  motorLeft.move(-move);
   motorRight.move(-move);
 }
 
-void curvaDir(int move) { //Curva para direita em um eixo
-  motorLeft.move(-move);
+void curvaDir (int move) { //Curva para direita em um eixo
+  motorLeft.move(move);
   motorRight.move(move);
 }
 
@@ -29,6 +29,20 @@ void setup(){
   Serial.begin(115200);
 }
 void loop(){
-frente(70);
+  while(millis()<5000){
+  frente(100);
+  }
+  while(millis()<10000){
+  statico();
+  }
+  while(millis()<15000){
+  curvaDir(100);
+  }
 
+  while(millis()<20000){
+  curvaEsq(100);
+  }
+  
+  statico();
+  statico();
 }
