d129ee45ca06da07248c05e7d1c0a743ba6d0420
Matheus Gabriel <math.gsilva07@gmail.com>
2023-06-10 21:08:59 -0300

    fix: organized variables

--- #Sunshine/#Sunshine.ino
+++ #Sunshine/#Sunshine.ino
@@ -1,51 +1,62 @@
 #include <Encoder.h>
 #include <HCSR04.h>
 #include <QTRSensors.h>
-#define "button.h"
+
 //Sensor de Giroscópico
 #include <MPU6050_tockn.h>
 #include <Wire.h>
+
 MPU6050 mpu6050(Wire);
 long timer = 0;
-float gyro;
+double gyro;
+int alvo;
 
 //DECLARAÇÃO DOS SENSORES DE REFLETÂNCIA
 QTRSensors qtr;
 
-const uint8_t SensorCount = 8;
+const uint8_t SensorCount = 4;
 uint16_t sensorValues[SensorCount];
 
+//DECLARAÇÃO DOS SENSORES DE REFLETÂNCIA
+//Esquerda para direita
+#define sensor1 A12
+#define sensor2 A11
+#define sensor3 A9
+#define sensor4 A8
+
 //DECLARAÇÃO DOS SENSORES DE COR
-#define cor1 A9  //CABO AZUL
-#define cor2 A8  //CABO VERDE
+#define cor1 A15  //CABO AZUL
+#define cor2 A14  //CABO VERDE
 
-//DECLARAÇÃO DOS MOTORES -> 05052022 INICIO &&
-
-#define G1 12   //motor GARRA
-#define G2 13   //motor GARRA
+//DECLARAÇÃO DOS MOTORES
+#define G1 12  //motor GARRA
+#define G2 13  //motor GARRA
 Encoder encoderM1(18, 19);
 Encoder encoderM2(20, 21);
 #define C1 10  //motor comporta
 #define C2 11  //motor comporta
 
-
-
-//declaração ultrassonicos
-
+//declaração ultrassonicos (Mudar portas)
 #define T_PIN_frente 24
 #define E_PIN_frente 25
 UltraSonicDistanceSensor u_frente(T_PIN_frente, E_PIN_frente);
 #define T_PIN_lat 26
 #define E_PIN_lat 27
-UltraSonicDistanceSensor u_lat(T_PIN_lat, E_PIN_lat);
+UltraSonicDistanceSensor u_Esq(T_PIN_lat, E_PIN_lat);
 #define T_PIN_late 28
 #define E_PIN_late 29
-UltraSonicDistanceSensor u_late(T_PIN_late, E_PIN_late);
+UltraSonicDistanceSensor u_Dir(T_PIN_late, E_PIN_late);
+#define T_PIN_late 30
+#define E_PIN_late 31
+UltraSonicDistanceSensor u_Top(T_PIN_late, E_PIN_late);
 
 boolean rampa = false;
 int ultima_medida, contador = 0;
 int dist_esq, dist_dir = 10000;
 int dist_garra = 20;
+
+
+// olhar o robô, , de frente para onde sobe a fiação dos motores. motor a direita, é o que está a sua direita. esquerda a mesma coisa, mas a esquerda
 
 
 //REGULAGEM DO ROBÔ COMEÇA AQUI
@@ -79,126 +90,199 @@ int larg_obs = 4750;        //largura do obstáculo
 
 //REGULAGEM DO ROBÔ ACABA AQUI
 
+//Classe do botão
+
+class button {
+  private:
+    byte debounce;
+    unsigned long nextRead;
+    bool _hasEstade;
+
+    void init() {
+      pinMode(pin, INPUT_PULLUP);
+    }
+
+  public:
+    byte pin;
+    bool estado;
+
+    button(byte _pin, byte _debounce = 10) {
+      this->pin = _pin;
+      this->debounce = _debounce;
+      this->init();
+    }
+
+    bool read() {
+      if (millis() > nextRead && digitalRead(pin) != estado) {
+        estado = !estado;
+        _hasEstade = true;
+        nextRead = millis() + debounce;
+      }
+      return estado;
+    }
+
+    bool hasEstade() {
+      read();
+      if (_hasEstade) {
+        _hasEstade = true;
+        return true;
+      }
+      return false;
+    }
+
+    bool pressed() {  //Se esta precionado
+      read();
+      return !estado;
+    }
+
+    bool released() {  //Se esta solto
+      read();
+      return estado;
+    }
+
+    bool risingEdge() {  //Indica se houve uma borda de subida
+      return !read() && hasEstade();
+    }
+
+    bool fallingEdge() {  //Indica se houve uma borda de descida
+      return read() && hasEstade();
+    }
+
+    void waitForPress(void (*doWhileWait)() = ([]() -> void {})) {  //Espera o botão ser pressionado enquando executa uma ação
+      while (!pressed()) {
+        doWhileWait();
+      }
+    }
+
+
+    void waitForRealease(void (*doWhileWait)() = ([]() -> void {})) {  //Espera o botão ser solto enquando executa uma ação
+      while (!released()) {
+        doWhileWait();
+      }
+    }
+
+    void waitForPressAndRealease(void (*doBeforePress)() = ([]() -> void {}), void (*doBeforeRealease)() = ([]() -> void {})) {  //Espera o botão ser pressionado enquanto executa uma ação e depois espera ser solto executando outra ação (lambda)
+      while (!pressed()) {
+        doBeforePress();
+      }
+      while (!released()) {
+        doBeforeRealease();
+      }
+    }
+};
+
+//Declaração dos botões
+button startButton(35);
+button bumper(37);
+button F1(36);
+button F2(38);
+button F3(40);
+
+//Classe led
+class led {
+  private:
+    unsigned long lastBlink;
+
+    void init() {
+      //Configura o pino do LED
+      pinMode(pin, OUTPUT);
+    }
+
+  public:
+    byte pin;    //Pino do led
+    bool state;  //Indica o estado do LED
+
+    led(byte _pin) {
+      this->pin = _pin;
+      this->init();
+    }
+
+    void set(bool _state) {
+      state = _state;
+      digitalWrite(pin, state);
+    }
+
+    /*
+          Liga o LED
+      */
+    void on() {
+      state = 1;
+      digitalWrite(pin, state);
+    }
+
+    /*
+        Desliga o LED
+      */
+    void off() {
+      state = 0;
+      digitalWrite(pin, state);
+    }
+
+    /*
+        Inverte o estado do LED
+      */
+    void toggle() {
+      state = !state;
+      digitalWrite(pin, state);
+    }
+
+    /*
+        Piscar LED
+      */
+    void blink(int timeout = 150) {
+      if (millis() < lastBlink + timeout) {
+        return;
+      }
+
+      this->toggle();
+      lastBlink = millis;
+    }
+};
+
+//Declaração dos LEDs
+led redLEDesq(27);
+led greenLEDcenter(28);
+led redLEDdir(29);
+
+
 void setup() {
-  // inicia serial. o serial, pode ser usado para acompanhar a leitura dos sensores
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 
-  start.waitForRealease{
-    loop();
-  }
+  //Sensores de refletancia
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
 
+  //Leds
+  redLEDesq.on();
+  greenLEDcenter.on();
+  redLEDdir.on();
 
-  /*
-    qtr.setTypeRC();
-    qtr.setSensorPins((const uint8_t[]){2,3, 4, 5, 6, 7, 8, 9}, SensorCount);
-    qtr.setEmitterPin(2);
+  //Botões
+  startButton.waitForRealease([]() -> void {
+    redLEDesq.blink();
+    greenLEDcenter.blink();
+    redLEDdir.blink();
+  });
 
-    int FT = 100;*/
-}
-
-void loop() {
-
-  mpu6050.update();
-  gyro = mpu6050.getGyroAngleZ();
-
-  while(!gyro > 88 && !gyro < 92){
-    curvaDir(40);
-  }
-  frear(0);
-}
-
-void debug() {
-  //Void de debug
-}
-
-
-/*
-  while (rampa == false) {
-
-
-
-    int leitura_verdade = leitura_sensor();          // leitura dos sensores de refletância, classificação de ligado/desligado pela tabela verdade
-    Serial.println(leitura_verdade);                 // imprime as leituras em serial
-    segue_linha(leitura_verdade);                    // sobe condição da leitura atual no segue linha
-
-
-
-    tempo_atual = millis();
-
-
-    tempo = tempo_atual - ultima_medida;
-  }
-    //Serial.println(tempo);
-   /*
-    if (tempo > 50) {
-      ultima_medida = tempo_atual;
-      tempo = 0;
-      obst = distancia_frente();
-      dist_dir = distancia_lateral();
-      dist_esq = esq_distancia_lateral();
-      Serial.println("entrei");
-        if (obst < distancia_parada && obst!=0 ) {
-          Serial.println("DESVIA");
-          desvia();
-        }
-
-
-
-        if (dist_dir < 10 || dist_esq < 10 && dist_dir != 0 && dist_esq != 0){
-          contador = contador + 1;
-          Serial.println(contador);
-          Serial.println(dist_dir);
-          Serial.println(dist_esq);
-          }
-          else{
-            contador = 0;
-            }
-
-            if (contador > 10){
-              rampa = true;
-              }
-
-
+  startButton.waitForPressAndRealease([]() -> void {
+    if (F1.pressed() && F3.pressed()) {
+      motorsCalibri(20);
+      //runCalibration();
     }
-
-    Serial.println(leitura_verdade);
-    Serial.println(obst);
-  }
-
-  while (rampa == true){
-
-    int leitura_verdade = leitura_sensor();          // leitura dos sensores de refletância, classificação de ligado/desligado pela tabela verdade
-    Serial.println(contador);
-    Serial.println("rampa");                 // imprime as leituras em serial
-    segue_rampa(leitura_verdade);                    // sobe condição da leitura atual no segue linha
-    tempo_atual = millis();
-    tempo = tempo_atual - ultima_medida;
-    if (tempo > 100) {
-      ultima_medida = tempo_atual;
-      tempo = 0;
-      dist_dir = distancia_lateral();
-      dist_esq = esq_distancia_lateral();
-      Serial.println(dist_esq);
-        if (dist_esq > 10 ){
-          contador = contador + 1;
-
-          }
-          else{
-            contador = 0;
-            }
-
-         if (contador > 10){
-           rampa = false;
-              }
+    if (F2.pressed()) {
+      debugLoop();
+      delay(50);
     }
-  }
+  });
 
-  while (rampa == false){
+  qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){ 2, 3, 4, 5, 6, 7, 8, 9 }, SensorCount);
+  qtr.setEmitterPin(2);
 
-  delay(10000);
-  resgate();
-  }
-*/
+  int FT = 100;
+}
