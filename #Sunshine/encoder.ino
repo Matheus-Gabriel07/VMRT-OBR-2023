fd6007c010fc4bf3a7917924c70169c258715d6b
Matheus Gabriel <math.gsilva07@gmail.com>
2023-06-10 21:12:33 -0300

    fix: update code

--- #Sunshine/encoder.ino
+++ #Sunshine/encoder.ino
@@ -1,48 +1,44 @@
-  void noventadireita()
-   {
-    
-    encoderM1.write(0);
-    // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
-    while (abs(encoderM1.read())<= curvanoventa) {
-      curvaDir(100);
+void noventadireita() {
+
+  encoderM1.write(0);
+  // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
+  while (abs(encoderM1.read()) <= curvanoventa) {
+    curvaDir(100);
     Serial.println(encoderM1.read());
-    }
-    frear(0);
-   }
+  }
+  frear(0);
+}
 
 
-   void noventaesquerda()
-   {
-    
-    encoderM1.write(0);
-    // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
-    while (abs(encoderM1.read())<= curvanoventaesqu) {
+void noventaesquerda() {
+
+  encoderM1.write(0);
+  // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
+  while (abs(encoderM1.read()) <= curvanoventaesqu) {
     curvaEsq(100);
     Serial.println(encoderM1.read());
-    }
-    frear(0);
-   }
+  }
+  frear(0);
+}
 
-   void quarentaecincoesquerda()
-   {
-    
-    encoderM1.write(0);
-    // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
-    while (abs(encoderM1.read())<= curvanoventaesqu / 1.8) {
+void quarentaecincoesquerda() {
+
+  encoderM1.write(0);
+  // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
+  while (abs(encoderM1.read()) <= curvanoventaesqu / 1.8) {
     curvaEsq(100);
     Serial.println(encoderM1.read());
-    }
-    frear(0);
-   }
+  }
+  frear(0);
+}
 
-   void quarentaecincodireita()
-   {
-    
-    encoderM1.write(0);
-    // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
-    while (abs(encoderM1.read())<= curvanoventa / 2) {
+void quarentaecincodireita() {
+
+  encoderM1.write(0);
+  // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
+  while (abs(encoderM1.read()) <= curvanoventa / 2) {
     curvaEsq(100);
     Serial.println(encoderM1.read());
-    }
-    frear(0);
-   }
+  }
+  frear(0);
+}
