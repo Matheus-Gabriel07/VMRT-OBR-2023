27497776c14339f816fddb5e3c73c65cbdb39a70
Matheus Gabriel <math.gsilva07@gmail.com>
2023-06-17 12:24:30 -0300

    fix: rename code and update void loop

--- Sunshine/Sunshine.ino
+++ Sunshine/Sunshine.ino
@@ -12,42 +12,38 @@ double gyro;
 int alvo;
 
 //DECLARAÇÃO DOS SENSORES DE REFLETÂNCIA
-QTRSensors qtr;
-
-const uint8_t SensorCount = 4;
-uint16_t sensorValues[SensorCount];
-
-//DECLARAÇÃO DOS SENSORES DE REFLETÂNCIA
-//Esquerda para direita
 #define sensor1 A12
 #define sensor2 A11
 #define sensor3 A9
 #define sensor4 A8
+#define sensorMeio A10
 
 //DECLARAÇÃO DOS SENSORES DE COR
-#define cor1 A15  //CABO AZUL
-#define cor2 A14  //CABO VERDE
+#define cor1 A14  //CABO AZUL
+#define cor2 A15  //CABO VERDE
 
-//DECLARAÇÃO DOS MOTORES
-#define G1 12  //motor GARRA
-#define G2 13  //motor GARRA
+//DECLARAÇÃO DOS MOTORES 
+
+#define G1 12   //motor GARRA
+#define G2 13   //motor GARRA
 Encoder encoderM1(18, 19);
 Encoder encoderM2(20, 21);
 #define C1 10  //motor comporta
 #define C2 11  //motor comporta
 
 //declaração ultrassonicos (Mudar portas)
-#define T_PIN_frente 24
-#define E_PIN_frente 25
+
+#define T_PIN_frente 45
+#define E_PIN_frente 44
 UltraSonicDistanceSensor u_frente(T_PIN_frente, E_PIN_frente);
-#define T_PIN_lat 26
-#define E_PIN_lat 27
+#define T_PIN_lat 51
+#define E_PIN_lat 50
 UltraSonicDistanceSensor u_Esq(T_PIN_lat, E_PIN_lat);
-#define T_PIN_late 28
-#define E_PIN_late 29
+#define T_PIN_late 48
+#define E_PIN_late 49
 UltraSonicDistanceSensor u_Dir(T_PIN_late, E_PIN_late);
-#define T_PIN_late 30
-#define E_PIN_late 31
+#define T_PIN_late 46
+#define E_PIN_late 47
 UltraSonicDistanceSensor u_Top(T_PIN_late, E_PIN_late);
 
 boolean rampa = false;
@@ -62,15 +58,17 @@ int dist_garra = 20;
 //REGULAGEM DO ROBÔ COMEÇA AQUI
 float GiroX = 0.0;  // GIRO os copium X
 float GiroZ = 0.0;  // Giro os copium Z
-int preto = 750;    //define valor de refletância para o preto. mudar aqui, após testar os sensores
-int corte = 990;    //define valor que o sensor de refletância, descarta. usamos para acertar os redutores de velocidade
-int l_linha = 280;  //define extensão da linha, perpendicularmente ao robô. tempo que o robô tem que andar, para ultrapassar uma linha em um cruzamento
+int preto = 650;    //define valor de refletância para o preto. mudar aqui, após testar os sensores
+int corte = 905;    //define valor que o sensor de refletância, descarta. usamos para acertar os redutores de velocidade
+int l_linha = 300;  //define extensão da linha, perpendicularmente ao robô. tempo que o robô tem que andar, para ultrapassar uma linha em um cruzamento
 int ajuste = 100;   //distância que regula a posição do sensor de cor
-int verde1 = 805;
-int verde2 = 375;        //leitura do verde
-int corte_verde1 = 236;  //corte para o verde
-int corte_verde2 = 175;  //corte para o verde
+int verde1 = 820;
+int verde2 = 880;        //leitura do verde
+int corte_verde1 = 10000;  //corte para o verde
+int corte_verde2 = 10000;  //corte para o verde
 //Velocidade dos Motores
+int vel_motor = 20;
+int vel_curva=15;
 int FG = 150;
 int FC = 100;
 int tempo_atual = 0;
@@ -93,16 +91,16 @@ int larg_obs = 4750;        //largura do obstáculo
 //Classe do botão
 
 class button {
-private:
-  byte debounce;
-  unsigned long nextRead;
-  bool _hasEstade;
+  private:
+    byte debounce;
+    unsigned long nextRead;
+    bool _hasEstade;
 
-  void init() {
-    pinMode(pin, INPUT_PULLUP);
-  }
+    void init() {
+      pinMode(pin, INPUT_PULLUP);
+    }
 
-public:
+  public:
   byte pin;
   bool estado;
 
@@ -173,77 +171,70 @@ public:
 
 //Declaração dos botões
 button startButton(43);
-button bumper(41);
+button bumper(42);
 button F1(39);
 button F2(37);
 button F3(35);
 
 //Classe led
 class led {
-private:
-  unsigned long lastBlink;  // Última vez que o LED foi piscado
+  private:
+    unsigned long lastBlink;
 
-  void init() {
-    // Configura o pino do LED
-    pinMode(pin, OUTPUT);
-  }
+    void init() {
+      //Configura o pino do LED
+      pinMode(pin, OUTPUT);
+    }
 
-public:
-  byte pin;    // Pino do LED
-  bool state;  // Indica se o LED está ligado ou desligado
+  public:
+    byte pin;     //Pino do led
+    bool state;      //Indica o estado do LED
 
-  /**
-     * @brief Construtor do LED
-     *
-     * @param pin: (byte) Pino do LED
-     */
-  led(byte _pin) {
-    this->pin = _pin;
-    this->init();
-  }
+    led(byte _pin) {
+      this-> pin = _pin;
+      this-> init();
+    }
 
-  /**
-     * @brief Seta um estado para o led
-     * @param _state: (bool) Estado do LED
-     *
-     * @example
-     *     meuLed.setState(true);
-     *    // LED ligado
-     */
-  void set(bool _state) {
-    state = _state;
-    digitalWrite(pin, state);
-  }
+    void set(bool _state) {
+      state = _state;
+      digitalWrite(pin, state);
+    }
 
-  /**
-     * @brief Liga o LED
-     */
-  void on() {
-    state = 1;
-    digitalWrite(pin, state);
-  }
+    /*
+        Liga o LED
+    */
+    void on(){
+      state = 1;
+      digitalWrite(pin, state);
+    }
 
-  /**
-     * @brief Desliga o LED
-     */
-  void off() {
-    state = 0;
-    digitalWrite(pin, state);
-  }
+    /*
+       Desliga o LED
+    */
+    void off(){
+      state = 0;
+      digitalWrite(pin, state);
+    }
 
-  /**
-     * @brief Inverte o estado do led
-     */
-  void toggle() {
-    state = !state;
-    digitalWrite(pin, state);
-  }
+    /*
+       Inverte o estado do LED
+    */
+    void toggle() {
+      state = !state;
+      digitalWrite(pin, state);
+    }
 
-  /**
-     * @brief Pisca o LED
-     *
-     * @param delay: (int) Tempo de espera entre os pulsos
-     */
+    /*
+       Piscar LED
+    */
+    void blink(int timeout = 150) {
+      if (millis() < lastBlink + timeout) {
+        return;
+      }
+
+      this->toggle();
+      lastBlink = millis;
+    }
 };
 
 //Declaração dos LEDs
@@ -251,48 +242,76 @@ led redLEDesq(36);
 led greenLEDcenter(34);
 led redLEDdir(32);
 
-void blink(int time = 200) {
-  bool state = true;
-  while (state != false) {
-    redLEDesq.on();
-    greenLEDcenter.on();
-    redLEDdir.on();
-    delay(time);
-    redLEDesq.off();
-    greenLEDcenter.off();
-    redLEDdir.off();
-    delay(time);
-  }
-}
+// Amarzenamento
+int arrayCor1[256] = {0};
+int arrayCor2[256] = {0};
+unsigned char posIn1 = 0, posIn2 = 0;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
+  
+  setupMotor(0);
 
   //Sensores de refletancia
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
   pinMode(sensor3, INPUT);
   pinMode(sensor4, INPUT);
-    pinMode(cor1, INPUT);
-  pinMode(cor2, INPUT);
+  pinMode(sensorMeio, INPUT);
 
   //Leds
-  blink();
+  redLEDesq.on();
+  greenLEDcenter.on();
+  redLEDdir.on();
 
-  //Botões
+  /*
+    qtr.setTypeRC();
+    qtr.setSensorPins((const uint8_t[]){2,3, 4, 5, 6, 7, 8, 9}, SensorCount);
+    qtr.setEmitterPin(2);
 
+    int FT = 100;*/
+}
 
-  qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){ 2, 3, 4, 5, 6, 7, 8, 9 }, SensorCount);
-  qtr.setEmitterPin(2);
-
-  int FT = 100;
+void debugLoop() {
+  Serial.print("sensor1: ");
+  Serial.print(analogRead(sensor1));
+  Serial.print(" ") ; 
+  Serial.print("sensor2: ");
+  Serial.print (analogRead(sensor2));
+  Serial.print(" ");
+  Serial.print("sensor3: ");
+  Serial.print(analogRead(sensor3));
+  Serial.print(" ");
+  Serial.print("sensor4");
+  Serial.print(analogRead(sensor4));
+  Serial.print("sensorMeio: ");
+  Serial.print(analogRead(sensorMeio));
+  Serial.print(" ");
+  Serial.print("sensorCor1: ");
+  Serial.print(analogRead(cor1));
+  Serial.print(" ");
+  Serial.print("sensorCor2: ");
+  Serial.print(analogRead(cor2));
+  Serial.print(" ");
+  Serial.println(" "); 
 }
 
 void loop() {
+  aguardo();
+
+  if(F2.pressed()){
+    debugLoop();
+  }
+  
+  if(F1.pressed()){
+    run();
+  }
+}
+
+/*
   while (rampa == false) {
 
 
@@ -308,4 +327,74 @@ void loop() {
 
     tempo = tempo_atual - ultima_medida;
   }
-}
+    //Serial.println(tempo);
+   /*
+    if (tempo > 50) {
+      ultima_medida = tempo_atual;
+      tempo = 0;
+      obst = distancia_frente();
+      dist_dir = distancia_lateral();
+      dist_esq = esq_distancia_lateral();
+      Serial.println("entrei");
+        if (obst < distancia_parada && obst!=0 ) {
+          Serial.println("DESVIA");
+          desvia();
+        }
+
+
+
+        if (dist_dir < 10 || dist_esq < 10 && dist_dir != 0 && dist_esq != 0){
+          contador = contador + 1;
+          Serial.println(contador);
+          Serial.println(dist_dir);
+          Serial.println(dist_esq);
+          }
+          else{
+            contador = 0;
+            }
+
+            if (contador > 10){
+              rampa = true;
+              }
+
+
+    }
+
+    Serial.println(leitura_verdade);
+    Serial.println(obst);
+  }
+
+  while (rampa == true){
+
+    int leitura_verdade = leitura_sensor();          // leitura dos sensores de refletância, classificação de ligado/desligado pela tabela verdade
+    Serial.println(contador);
+    Serial.println("rampa");                 // imprime as leituras em serial
+    segue_rampa(leitura_verdade);                    // sobe condição da leitura atual no segue linha
+    tempo_atual = millis();
+    tempo = tempo_atual - ultima_medida;
+    if (tempo > 100) {
+      ultima_medida = tempo_atual;
+      tempo = 0;
+      dist_dir = distancia_lateral();
+      dist_esq = esq_distancia_lateral();
+      Serial.println(dist_esq);
+        if (dist_esq > 10 ){
+          contador = contador + 1;
+
+          }
+          else{
+            contador = 0;
+            }
+
+         if (contador > 10){
+           rampa = false;
+              }
+    }
+  }
+
+  while (rampa == false){
+
+  delay(10000);
+  resgate();
+  }
+*/
